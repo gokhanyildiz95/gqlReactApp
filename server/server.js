592a416605efa727717438db8bd27ad7fadff7bc
gokhan yildiz <gokhan.yildiz@skyz.tech>
2021-03-14 00:19:21 +0300

    start project and add mutation data

---
+++ server/server.js
@@ -0,0 +1,58 @@
+const {ApolloServer} = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose=require('mongoose');
+const MakaleModel = require('./models/MakaleModel');
+
+const DB_URI = 'mongodb+srv://gkhn:1234@blogcluster.dafwe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const typeDefs=gql`
+
+    type Makale{
+        id:ID!,
+        baslik:String!,
+        icerik:String!,
+    }
+
+    type Query{
+        makalelerGetir:[Makale]!
+    }
+
+    type Mutation{
+        makaleOlustur(baslik:String!,icerik:String!):Makale!
+    }
+`;
+
+const resolvers={
+
+    Query:{
+        async makalelerGetir(){
+            const makaleler=await MakaleModel.find();
+            return makaleler;
+        }
+    },
+    Mutation:{
+        makaleOlustur:async (parent,args)=>{
+            try{
+                const makale={
+                    baslik:args.baslik,
+                    icerik:args.icerik
+                };
+                return await MakaleModel.create(makale); 
+            } catch(error) {
+                throw new error;
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose.connect(DB_URI,{useNewrlParser:true,useUnifiedTopology:true}).then(()=>{
+    console.log('mongodb bağlantısı başarılı');
+    return server.listen({port:5000})
+}).then((res)=>{
+    console.log(`server ${res.url} adresinde çalışıyor `);
+})
